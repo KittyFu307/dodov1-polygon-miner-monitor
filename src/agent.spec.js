919c25932f4f946d12bf561717ee47d87e350fdb
kitty <2640854417@qq.com>
2022-06-10 15:24:47 +0800

    update test

--- src/agent.spec.js
+++ src/agent.spec.js
@@ -6,10 +6,7 @@ const {
   REWARD_TOKEN,
   REWARD_VAULT,
 } = require("./constants");
-
 const mockBalance = 10**18;
-
-
 // mock the JsonRpcBatchProvider and Contract constructors
 jest.mock('ethers', () => ({
   Contract: jest.fn(),
@@ -30,7 +27,6 @@ describe('dodo vault address token monitoring', () => {
     var mockProvider = jest.fn();
     var mockTokenContract = {
       balanceOf: jest.fn(),
-      transfer: jest.fn()
     };
 
     var resetMocks = function () {
@@ -39,9 +35,19 @@ describe('dodo vault address token monitoring', () => {
     };
 
     const logsMatchTransferEventAddressMatch = [{
-      address: '0x43Dfc4159D86F3A37A5A4B3D4580b888ad7d4DDd',
+      address: '0xB14dA65459DB957BCEec86a79086036dEa6fc3AD',
       topics: [
-        '0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef',
+        '0xf279e6a1f5e320cca91135676d9cb6e44ca8a08c0b88342bcdb1144f6511b568',
+        ethers.constants.HashZero,
+        ethers.constants.HashZero,
+      ],
+      data: `xxxxx`,
+    }];
+
+    const logsNoMatchTransferEventAddressMatch = [{
+      address: '0xB14dA65459DB957BCEec86a79086036dEa6fc3AD',
+      topics: [
+        '0x90890809c654f11d6e72a28fa60149770a0d11ec6c92319d6ceb2bb0a4ea1a15',
         ethers.constants.HashZero,
         ethers.constants.HashZero,
       ],
@@ -54,12 +60,11 @@ describe('dodo vault address token monitoring', () => {
     });
 
     it('returns empty findings for the amount greater than the minimum amount', async () => {
-      // supply ethers.Contract mock with correct mock contract objects to return
       ethers.Contract = jest.fn()
           .mockImplementationOnce(() => mockTokenContract);
       mockTokenContract.balanceOf.mockReturnValue(10**28);
       const receipt = {
-        logs: logsMatchTransferEventAddressMatch,
+        logs: logsNoMatchTransferEventAddressMatch,
       };
       const txEvent = createTransactionEvent({receipt})
       // this will determine that the Flash included an amount of 100 tokens of token0
@@ -81,16 +86,16 @@ describe('dodo vault address token monitoring', () => {
       const findings = await handleTransaction(txEvent);
       const expectedFindings = [
         Finding.fromObject({
-          name: 'DODO miner vault contract reward token less than 25,000,000',
-          description: `DODO miner vault contract reward token less than 25,000,000, the amount is 1`,
-          alertId: 'DODO-Miner-Vault-Token-Less-Than-BaseAmount',
+          name: 'DODO V1 miner Polygon：The reward token balance of reward vault on polygon',
+          description: `The reward token balance of reward vault on polygon, the amount is 1`,
+          alertId: 'DODO-V1-Miner-Vault-Token-Polygon',
           severity: FindingSeverity.Info,
           type: FindingType.Info,
           protocol: 'DODO',
           metadata: {
             vaultAddress: REWARD_VAULT.toString(),
             rewardToken: REWARD_TOKEN.toString(),
-            tokenBalance: "1"
+            tokenBalance: '1',
           },
         }),
       ];
